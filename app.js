cca00306f94beda4f8366532de18d89d10f3cc88
Geoff Fox <fox.geoffrey@gmail.com>
2022-04-26 16:11:29 -0700

    css

--- app.js
+++ app.js
@@ -2,24 +2,11 @@
  * File app.js 
  */
 const app = Vue.createApp({
-  //data: function() {...}
-  data:() => {
+  data() {
     return {
-      courseGoalA: 'Finish the course and learn Vue!!',
-      courseGoalB: 'Master Vue and build amazing apps!',
-      vueLink: 'https://vuejs.org/'
+      counter:0
     };
-  },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
-    }
-  }
+  } 
 });
 
-app.mount('#user-goal');
+app.mount('#events');
