210de619135520ce52679c3e4e9a899e166b888d
Geoff Fox <fox.geoffrey@gmail.com>
2022-04-26 17:17:14 -0700

    input text from input box

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:''
     };
   },
   methods: {
+    setName(event) {
+      // this straight Javascript '$event' is being passed
+      // note dev tool  F12 shows 'fashing' changes in code
+      this.name = event.target.value
+    },
     add(num) {
       this.counter += num;
     },
