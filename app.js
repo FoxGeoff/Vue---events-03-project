7ea507a40c773f2f615b02d38c50fc28e94df38c
Geoff Fox <fox.geoffrey@gmail.com>
2022-04-28 01:12:16 -0700

    Working with Watchers

--- app.js
+++ app.js
@@ -5,31 +5,39 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name:''
+      name: "test",
     };
+  },
+  watch: {
+    //exe when 'name' prop changes. no return.
+    // watch has name(newNameVal, oldNameVal) built in
+    // This would replace the computed property 'fullName'
+    name(val) {
+     this.fullName = val + " Shwali";
+    },
   },
   // These are funtion wihich act like methods. Avoiding reloads
   computed: {
     fullName() {
-      if(this.name === '') {
-        return '';
+      if (this.name === "") {
+        return "";
       }
-      return this.name + ' Fox';
-    }
+      return this.name + " Fox";
+    },
   },
   methods: {
     myFormSubmitBeter() {
-      alert('A better form submit');
+      alert("A better form submit");
     },
     myFormSubmit(event) {
-      alert('mySubmit');
+      alert("mySubmit");
       // stops form submit to server
       event.preventDefault();
     },
     setName(event) {
       // this straight Javascript '$event' is being passed
       // note dev tool  F12 shows 'fashing' changes in code
-      this.name = event.target.value
+      this.name = event.target.value;
     },
     add(num) {
       this.counter += num;
